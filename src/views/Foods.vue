1499696566bd3d5bc23b6aab73aa41d8150d3f12
Hendradedis <hendradedisupriadi@gmail.com>
2021-09-06 14:46:38 +0700

    first commit

---
+++ src/views/Foods.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar/>
+    <H2>Foods </H2>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Foods',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
